f3063bad35a753df9a096a979c5caf357bec751a
rickosong <ricko.aprilianto00@gmail.com>
2021-08-06 09:19:17 +0800

    learning object in javascript

---
+++ js-dasar/object.js
@@ -0,0 +1,33 @@
+// membuat object
+// object literal
+var siswa = {
+    nama : "Mohammad Ricko Aprilianto",
+    Umur : 17,
+    Nim : [92, 98, 40, 68],
+    alamat : {
+        jalan : "jl, abc rt.12",
+        kota : "Banjarmasin",
+        provinsi : "kalimantan Selatan",
+    }
+};
+
+// function declaration
+function buatObjectSiswa(nama, umur, nim, jurusan) {
+    var siswa = {};
+    siswa.nama = nama;
+    siswa.umur = umur;
+    siswa.nim = nim;
+    siswa.jurusan = jurusan;
+
+    return siswa;
+}
+
+var siswa3 = buatObjectSiswa('Ricko', '17', '90', 'rpl');
+
+// constructor
+function Siswa(nama, umur) {
+    this.nama = nama;
+    this.umur = umur;
+}
+
+var siswa4 = new Siswa('Rico', '17');
